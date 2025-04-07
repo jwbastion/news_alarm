bb3193b35261cb458d6e74ed3ec7bbb2bda22126
kwonyoun <kyjyj6334@gmail.com>
2025-04-07 19:52:15 +0900

    run_all 수정 commit

--- run_all.py
+++ run_all.py
@@ -1,24 +1,27 @@
 import subprocess
 from datetime import datetime
 
-print("[1/5] 유튜브 영상 URL 수집 중...")
-subprocess.run(["python", "miniproject.py"], check=True)
-
-print("\n[2/5] Whisper로 텍스트 변환 중...")
-subprocess.run(["python", "url_whisper.py"], check=True)
-
-print("\n[3/5] LangChain으로 뉴스 요약 중...")
-subprocess.run(["python", "summary.py"], check=True)
-
-print("\n[4/5] 워드클라우드 생성 중...")
-subprocess.run(["python", "wordcloud.py"], check=True)
 
 # 실행 로그 남기기
 with open("실행로그.txt", "a", encoding="utf-8") as f:
     f.write(f"실행됨: {datetime.now()}\n")
 
-# GitHub 자동 커밋 & 푸시
-print("\n[5/5] GitHub에 자동 푸시 중...")
-import push_to_github
+print("[1/6] 유튜브 영상 URL 수집 중...")
+subprocess.run(["python", "miniproject.py"], check=True)
 
-print("\n전체 자동화 완료! 생성된 CSV, 워드클라우드 파일 확인 및 GitHub 업로드 완료.")
+print("\n[2/6] Whisper로 텍스트 변환 중...")
+subprocess.run(["python", "url_whisper.py"], check=True)
+
+print("\n[3/6] LangChain으로 뉴스 요약 중...")
+subprocess.run(["python", "summary.py"], check=True)
+
+print("\n[4/6] 워드클라우드 생성 중...")
+subprocess.run(["python", "generate_wordcloud.py"], check=True)
+
+print("\n[5/6] GitHub에 자동 푸시 중...")
+subprocess.run(["python", "git.py"], check=True)
+
+print("\n[6/6] Slack Bot 전송 중...")
+subprocess.run(["python", "slack_bot.py"], check=True)
+
+print("\n전체 자동화 완료! GitHub 업로드 및 SlackBot 전송송 완료.")
